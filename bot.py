88fff3bd9219c559203ebe85b7e7895330fc76c7
Peter-Dong1 <peterdong1951@gmail.com>
2024-07-25 12:22:32 -0400

    changed port again

--- bot.py
+++ bot.py
@@ -374,10 +374,10 @@ def parse_arguments():
 
     if args.production:
         args.exchange_hostname = "production"
-        args.port = 20000
+        args.port = 22000
     elif args.test:
         args.exchange_hostname = "test-exch-" + team_name
-        args.port = 20000 + test_exchange_port_offsets[args.test]
+        args.port = 22000+ test_exchange_port_offsets[args.test]
         if args.test == "empty":
             args.add_socket_timeout = False
     elif args.specific_address:
